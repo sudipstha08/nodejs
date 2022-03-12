5a6df651dc7a41e378d4849bb1943cbb134847c5
Sudip Shrestha <sudipstha08@gmail.com>
2022-03-12 22:16:04 +0545

    add view engine

--- app1/README.md
+++ app1/README.md
@@ -5,5 +5,9 @@
 | Commands | Desp |
 |-----|----|
 |npm run devStart|start dev server|
+
+
+## Ejs
+- View Engine
 ## References
 1. https://nodejs.org/en/docs/
--- app1/server.js
+++ app1/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app =express()
 
+app.set('view engine', 'ejs')
+
 // Setting Routes
 app.get('/', (req, res) => {
   console.log('Here')
   // res.sendStatus(500)
   // res.send('Hi')
   // res.status(500).send("Oops server failed")
-  res.status(500).json({ message: "Errors"})
+  // res.status(500).json({ message: "Errors"})
+
+  // Download file
+  // res.download("server.js")
+
+  // Render file
+  res.render('index')
 })
 
 app.listen(3000)

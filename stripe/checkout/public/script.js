2b655a1f7fd1797551d8f473069864e441f8051c
sudipstha08 <sudipstha08@gmail.com>
2023-08-26 23:15:10 +0545

    stripe checkout

---
+++ stripe/checkout/public/script.js
@@ -0,0 +1,36 @@
+const button = document.querySelector("button");
+
+console.log("button",button)
+
+button.addEventListener("click", () => {
+	console.log("Checkout");
+	fetch("/create-checkout-session", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			items: [
+				{
+					id: 1,
+					quantity: 2,
+				},
+				{
+					id: 2,
+					quantity: 1,
+				},
+			],
+		}),
+	})
+		.then((res) => {
+			if (res.ok) return res.json();
+			return res.json().then((json) => Promise.reject(json));
+		})
+		.then(({ url }) => {
+			console.log(url)
+			window.location = url;
+		})
+		.catch((e) => {
+			console.error(e.error);
+		});
+});
--- stripe/checkout/server.js
+++ stripe/checkout/server.js
@@ -25,4 +25,31 @@ const storeItems = new Map([
 	],
 ]);
 
+app.post("/create-checkout-session", async (req, res) => {
+	try {
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ["card"],
+			mode: 'payment',
+			line_items: req.body.items.map(item => {
+				const storeItem = storeItems.get(item.id)
+				return {
+					price_data: {
+						currency: 'usd',
+						product_data: {
+							name: storeItem.name
+						},
+						unit_amount: storeItem.priceInCents
+					},
+					quantity: item.quantity
+				}
+			}),
+			success_url: `${process.env.SERVER_URL}/success.html`,
+			cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+		});
+		res.json({ url: session.url });
+	} catch (e) {
+		res.status(500).json({ error: e.message });
+	}
+});
+
 app.listen(9000);

73b0e0f173314bae5075b8ae59c55f69cf48a8c3
sudipstha08 <sudipstha08@gmail.com>
2024-01-29 21:34:03 +0545

    feat: add transcriptions

--- paypal/app.js
+++ paypal/app.js
@@ -1,54 +1,68 @@
-const express = require('express')
-const paypal = require('paypal-rest-sdk');
-const { sendPayout, getPayout } = require('./controllers/index.js');
-require('ejs')
-require('dotenv').config()
+const express = require("express");
+const paypal = require("paypal-rest-sdk");
+const axios = require('axios');
+const multer = require('multer');
+const fs = require('fs')
+
+const {
+	sendPayout,
+	getPayout,
+	handleOpenAi,
+} = require("./controllers/index.js");
+require("ejs");
+require("dotenv").config();
 
 paypal.configure({
-	'mode': 'sandbox', //sandbox or live
-	'client_id': process.env.PAYPAL_CLIENT_ID,
-	'client_secret': process.env.PAYPAL_CLIENT_SECRET
+	mode: "sandbox", //sandbox or live
+	client_id: process.env.PAYPAL_CLIENT_ID,
+	client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
-const app = express()
+const app = express();
 const PORT = 9001;
 
-app.set('view engine', "ejs")
+app.set("view engine", "ejs");
 
-app.get('/payment', (req, res) => res.render('payment'))
-app.get('/payout', (req, res) => res.render('payout'))
+app.get("/payment", (req, res) => res.render("payment"));
+app.get("/payout", (req, res) => res.render("payout"));
 
-app.get("/", (req, res) => res.status(200).send(`Server listening at port: ${PORT}`))
+app.get("/", (req, res) =>
+	res.status(200).send(`Server listening at port: ${PORT}`)
+);
 
 /**
  * Handle payments
  */
-app.post('/pay', (req, res) => {
+app.post("/pay", (req, res) => {
 	const create_payment_json = {
-		"intent": "sale",
-		"payer": {
-			"payment_method": "paypal"
+		intent: "sale",
+		payer: {
+			payment_method: "paypal",
 		},
-		"redirect_urls": {
-			"return_url": "http://localhost:3000/success",
-			"cancel_url": "http://localhost:3000/cancel"
+		redirect_urls: {
+			return_url: "http://localhost:3000/success",
+			cancel_url: "http://localhost:3000/cancel",
 		},
-		"transactions": [{
-			"item_list": {
-				"items": [{
-					"name": "item",
-					"sku": "item",
-					"price": "25.00",
-					"currency": "USD",
-					"quantity": 1
-				}]
+		transactions: [
+			{
+				item_list: {
+					items: [
+						{
+							name: "item",
+							sku: "item",
+							price: "25.00",
+							currency: "USD",
+							quantity: 1,
+						},
+					],
+				},
+				amount: {
+					currency: "USD",
+					total: "25.00",
+				},
+				description: "Hat for the best team ever",
 			},
-			"amount": {
-				"currency": "USD",
-				"total": "25.00"
-			},
-			"description": "Hat for the best team ever"
-		}]
+		],
 	};
 	paypal.payment.create(create_payment_json, function (error, payment) {
 		if (error) {
@@ -59,49 +73,67 @@ app.post('/pay', (req, res) => {
 			// res.status(200).send('test payment')
 			for (let i = 0; i < payment.links.length; i++) {
 				if (payment.links[i].rel === "approval_url") {
-					res.redirect(payment.links[i].href)
+					res.redirect(payment.links[i].href);
 				}
 			}
 		}
 	});
+});
 
-})
-
-app.get('/success', (req, res) => {
-	const payerId = req.query.PayerID
-	const paymentId = req.query.paymentId
+app.get("/success", (req, res) => {
+	const payerId = req.query.PayerID;
+	const paymentId = req.query.paymentId;
 
 	const execute_payment_json = {
-		"payer_id": payerId,
-		"transactions": [{
-			"amount": {
-				"currency": "USD",
-				"total": "25.00"
-			}
-		}]
-	}
+		payer_id: payerId,
+		transactions: [
+			{
+				amount: {
+					currency: "USD",
+					total: "25.00",
+				},
+			},
+		],
+	};
 
 	/**
 	 * Payment is done to the business account selected during app creation
 	 */
-	paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-		if (error) {
-			console.log(error.response);
-			throw error;
-		} else {
-			console.log("Get Payment Response");
-			console.log(JSON.stringify(payment));
-			res.status(200).send("success")
+	paypal.payment.execute(
+		paymentId,
+		execute_payment_json,
+		function (error, payment) {
+			if (error) {
+				console.log(error.response);
+				throw error;
+			} else {
+				console.log("Get Payment Response");
+				console.log(JSON.stringify(payment));
+				res.status(200).send("success");
+			}
 		}
-	});
-})
+	);
+});
 
-app.get("/cancel", (req, res) => res.status(200).send("Cancelled"))
+app.get("/cancel", (req, res) => res.status(200).send("Cancelled"));
+
+// Multer setup
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "uploads/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+const upload = multer({ storage: multer.memoryStorage() });
 
 /**
  * Handle payouts
  */
-app.post('/payout', sendPayout)
-app.post('/get-payout', getPayout)
+app.post("/payout", sendPayout);
+app.post("/get-payout", getPayout);
 
-app.listen(PORT || 9000, () => console.log(`Server started at PORT: ${PORT}`))
+app.post("/openai", upload.single('audio'), handleOpenAi);
+
+app.listen(PORT || 9000, () => console.log(`Server started at PORT: ${PORT}`));
--- paypal/controllers/index.js
+++ paypal/controllers/index.js
@@ -1,97 +1,111 @@
-const paypal = require('paypal-rest-sdk');
+const { default: axios } = require("axios");
+const paypal = require("paypal-rest-sdk");
+const fs = require("fs");
 
-function fetchPayoutDetails (payoutId) {
-  return new Promise(function(resolve,reject) {
-     paypal.payout.get(payoutId, async function (error, payout) {
-      if (error) {
-         console.log("Error while fetching payout details: ",error);
-         reject(error)
-      } else {
-         console.log("Get Payout Details Response: ", payout);
-         resolve(payout)
-      }
-   });
-  })
-}
-
-/**
- * 
- * @param req 
- * @param res 
- * SendPayout => Send single payout
- */
- const sendPayout =async (req, res) => {
- 	/**
-     * Create unique batch ID to prevent duplicate batch from being sent
-	  * ---------------------------------------------------------------------
-	  * PayPal prevents duplicate batches from being processed. 
-	  * If you specify a sender_batch_id that was used in the last 30 days, 
-	  * the API rejects the request and returns an error message 
-	  * that indicates the duplicate sender_batch_id and includes a HATEOAS link 
-	  * to the original batch payout with the same sender_batch_id. 
-	  * If you receive an HTTP 5nn status code, you can safely retry the request
-	  * with the same sender_batch_id. The API completes a payment only once for 
-	  * a specific sender_batch_id that is used within 30 days.
-     */	
-	const sender_batch_id = (Math.random() * 0xfffff * 1000000000).toString(16).slice(0, 13).toUpperCase()	
-
-	const create_payout_json = {
-			"sender_batch_header": {
-					"sender_batch_id": sender_batch_id,
-					"email_subject": "You have a payment for your sales",
-					"email_message": "You have received a payouts"
-			},
-			"items": [
-					{
-							"recipient_type": "EMAIL",
-							"amount": {
-									"value": 1,
-									"currency": "USD"
-							},
-							"receiver": "sb-je5vc24921338@personal.example.com",
-							"note": "Your sales has been paid",
-							"sender_item_id": "fsjfsk3434r5"
-					}
-			]
-	};
-	
-	/**
-	 * SYNC_MODE is deprecated
-	 */
-	const sync_mode = 'false';
-	
-	 paypal.payout.create(create_payout_json, sync_mode, async function (error, payout) {
+function fetchPayoutDetails(payoutId) {
+	return new Promise(function (resolve, reject) {
+		paypal.payout.get(payoutId, async function (error, payout) {
 			if (error) {
-					console.log(error);
-					res.status(400).send(`Error occured, ${error}`)
+				console.log("Error while fetching payout details: ", error);
+				reject(error);
 			} else {
-					console.log("Create Single Payout Response");
-					console.log(payout);
-          console.log("SENDER BATCH ID: ", payout.batch_header.sender_batch_header.sender_batch_id)
-					const payoutDetails =  await fetchPayoutDetails(payout?.batch_header?.payout_batch_id)
-          console.log("payoutDetails===>", payoutDetails)
-					res.status(200).send({
-						data: {payout, payoutDetails},
-					})
+				console.log("Get Payout Details Response: ", payout);
+				resolve(payout);
 			}
+		});
 	});
 }
 
+/**
+ *
+ * @param req
+ * @param res
+ * SendPayout => Send single payout
+ */
+const sendPayout = async (req, res) => {
+	/**
+	 * Create unique batch ID to prevent duplicate batch from being sent
+	 * ---------------------------------------------------------------------
+	 * PayPal prevents duplicate batches from being processed.
+	 * If you specify a sender_batch_id that was used in the last 30 days,
+	 * the API rejects the request and returns an error message
+	 * that indicates the duplicate sender_batch_id and includes a HATEOAS link
+	 * to the original batch payout with the same sender_batch_id.
+	 * If you receive an HTTP 5nn status code, you can safely retry the request
+	 * with the same sender_batch_id. The API completes a payment only once for
+	 * a specific sender_batch_id that is used within 30 days.
+	 */
+	const sender_batch_id = (Math.random() * 0xfffff * 1000000000)
+		.toString(16)
+		.slice(0, 13)
+		.toUpperCase();
+
+	const create_payout_json = {
+		sender_batch_header: {
+			sender_batch_id: sender_batch_id,
+			email_subject: "You have a payment for your sales",
+			email_message: "You have received a payouts",
+		},
+		items: [
+			{
+				recipient_type: "EMAIL",
+				amount: {
+					value: 1,
+					currency: "USD",
+				},
+				receiver: "sb-je5vc24921338@personal.example.com",
+				note: "Your sales has been paid",
+				sender_item_id: "fsjfsk3434r5",
+			},
+		],
+	};
+
+	/**
+	 * SYNC_MODE is deprecated
+	 */
+	const sync_mode = "false";
+
+	paypal.payout.create(
+		create_payout_json,
+		sync_mode,
+		async function (error, payout) {
+			if (error) {
+				console.log(error);
+				res.status(400).send(`Error occured, ${error}`);
+			} else {
+				console.log("Create Single Payout Response");
+				console.log(payout);
+				console.log(
+					"SENDER BATCH ID: ",
+					payout.batch_header.sender_batch_header.sender_batch_id
+				);
+				const payoutDetails = await fetchPayoutDetails(
+					payout?.batch_header?.payout_batch_id
+				);
+				console.log("payoutDetails===>", payoutDetails);
+				res.status(200).send({
+					data: { payout, payoutDetails },
+				});
+			}
+		}
+	);
+};
+
 const getPayout = (req, res) => {
-   const payoutId = "HK5PZLAZ34RKN"
-   paypal.payout.get(payoutId, function (error, payout) {
-      if (error) {
-         console.log(error);
-			res.status(400).send(`Error occured, ${error}`)
-      } else {
-         console.log("Get Payout Response");
-         console.log(JSON.stringify(payout));
-         res.status(200).send({
-            data: payout
-         })
-      }
-   });
-}
+	const payoutId = "HK5PZLAZ34RKN";
+	paypal.payout.get(payoutId, function (error, payout) {
+		if (error) {
+			console.log(error);
+			res.status(400).send(`Error occured, ${error}`);
+		} else {
+			console.log("Get Payout Response");
+			console.log(JSON.stringify(payout));
+			res.status(200).send({
+				data: payout,
+			});
+		}
+	});
+};
 
 /**
  * res -> data -> link -> href => Shows the latest status of a batch payout. Includes the transaction status and other data for individual payout items.
@@ -99,5 +113,53 @@ const getPayout = (req, res) => {
 
 //HK5PZLAZ34RKN
 
-module.exports={ sendPayout, getPayout }
+const handleOpenAi = async (req, res) => {
+	try {
+		const audioFile = req.file;
+
+		console.log("req", audioFile);
+		const formData = new FormData();
+
+		const mimeType = audioFile.mimetype;
+		const fileExtension = mimeType.split("/")[1];
+		const filename = `${audioFile.fieldname}.${fileExtension}`;
+
+		const blob = new Blob([audioFile.buffer], { type: mimeType });
+
+		formData.append("file", blob, {
+			contentType: mimeType,
+			filename,
+		});
+
+		formData.append("model", "whisper-1");
+
+		const apiKey = process.env.OPEN_AI_KEY;
+
+		const response = await axios.post(
+			"https://api.openai.com/v1/audio/transcriptions",
+			formData,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+					"Content-Type": "multipart/form-data",
+				},
+			}
+		);
+
+		if (response) {
+			console.log("response", response?.data);
+			res.json({ transcription: response.data });
+		} else {
+			throw new Error("Failed to transcribe audio.");
+		}
+	} catch (error) {
+		console.error("Error transcribing audio:", error);
+		console.error("Error transcribing audio:", error?.response?.data);
+		res
+			.status(500)
+			.json({ error: "Failed to transcribe audio.", data: error?.response });
+	}
+};
+
+module.exports = { sendPayout, getPayout, handleOpenAi };
 //https://www.npmjs.com/package/paypal-rest-sdk

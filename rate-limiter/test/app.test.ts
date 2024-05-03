79b423474c50352e3506d90798a5d3690c6cf551
sudipstha08 <sudipstha08@gmail.com>
2024-05-03 23:12:32 +0545

    feat: add jest test cases

---
+++ rate-limiter/test/app.test.ts
@@ -0,0 +1,9 @@
+import supertest from "supertest";
+import app from "../app";
+import { describe } from "node:test";
+
+describe("POST /users", () => {
+	describe("given a username and password", () => {});
+
+	describe("when the username and password is missing", () => {});
+});
--- ts-app/app.ts
+++ ts-app/app.ts
@@ -1,4 +1,22 @@
-import { v1 as uuidv1 } from "uuid";
+import express from "express";
 
+const app = express();
 
-console.log(uuidv1())
+app.use(express.json());
+
+app.post("/login", (req, res) => res.send("OK"));
+
+app.post("/users", (req, res) => {
+	const { password, username } = req.body;
+	if (!password || !username) {
+		res.sendStatus(400);
+		return;
+	}
+	res.send({
+		userId: "fdsfsdf",
+	});
+});
+
+app.listen(3000, () => console.log("Server on port 3000"));
+
+export { app };
